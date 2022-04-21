e4299f7d58713f208902e0fe5c89e14e5aa175c8
Thundyy <mattiaf764@gmail.com>
2022-04-21 22:34:22 +0200

    Initial commit

---
+++ src/main/java/it/thundyy/advertise/AdvertisePlugin.java
@@ -0,0 +1,17 @@
+package it.thundyy.advertise;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvertisePlugin extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+    
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
